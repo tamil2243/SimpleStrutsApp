a0a65fd8edb0b802c5fa812ec1f6e1936ef58534
Tamilmani S <tamil-inc5570@zeroproxy.zohocorp.com>
2025-07-18 11:24:53 +0530

    Initial commit

--- WebContent/loginSuccess.jsp
+++ WebContent/loginSuccess.jsp
@@ -1,9 +1,13 @@
-<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ taglib uri="/struts-tags" prefix="s" %>
 <html>
 <head><title>LogIn Success</title></head>
 <body>
+	
 
 	<h2>Hello, <s:property value="name" />!</h2>
+	<h2>Hello, <s:property value="user.name" />!</h2>
+
+	<s:debug />   <%-- Shows all available OGNL context vars --%>
 	<s:set var="u" value="user"/>
 	<h2>Hello, <s:property value="#u.name"/></h2>
 
--- src/com/example/action/LoginAction.java
+++ src/com/example/action/LoginAction.java
@@ -1,4 +1,5 @@
 package com.example.action;
+import com.example.action.User;
 import java.util.Optional;
 import org.apache.struts2.ActionSupport;
 import org.apache.struts2.interceptor.parameter.StrutsParameter;
@@ -9,15 +10,18 @@ public class LoginAction extends ActionSupport{
 	private String password;
 	private User user;
 	private UserDAO userDAO=new UserDAO();
-
 	public String execute(){
 
 		Optional<User> optionalUser= userDAO.logIn(email,password);
 		if(optionalUser.isEmpty())return "failure";
 		user=optionalUser.get();
-		System.out.println("From login Action"); 
+		System.out.println("From login Action");
+		System.out.println("User's ID from DAO: " + user.getId());
+	        System.out.println("User's name from DAO: " + user.getName());
+		System.out.println("User's Email from DAO: " + user.getEmail());
 		return "success";
 	}
+
 	public User getUser(){return user;}
 	@StrutsParameter
 	public void setEmail(String email){
